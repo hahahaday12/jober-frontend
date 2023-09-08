42bce8c51f073892b3886119056d2d99d160658e
howooking <junsgk@gmail.com>
2023-09-08 15:15:17 +0900

    Doc: types, constants, dummy

---
+++ src/types/wall.ts
@@ -0,0 +1,33 @@
+import {
+  BlockType,
+  FileBlockType,
+  FreeBlockType,
+  ListBlockType,
+  SnsType,
+  StyleType,
+  TemplateType,
+} from './blocks';
+
+export type CategoryType =
+  | 'career'
+  | 'private'
+  | 'event'
+  | 'enterprise'
+  | 'basic';
+
+export interface WallType {
+  userId: number;
+  category: CategoryType;
+  shareUrl: string;
+  pageTitle: string;
+  pageDescription: string;
+  ProfileImageUrl: string | null;
+  profileBgUrl: string | null;
+  listBlock?: ListBlockType;
+  fileBlock?: FileBlockType;
+  snsBlock?: SnsType[];
+  freeBlock?: FreeBlockType;
+  templateBlock?: TemplateType[];
+  order: BlockType[];
+  style: StyleType;
+}
