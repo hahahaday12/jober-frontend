0bc57899457c16232eaed934e9dc2d9f7c016295
howooking <junsgk@gmail.com>
2023-09-08 15:40:47 +0900

    Feat: zustand store 생성

---
+++ src/store.ts
@@ -0,0 +1,27 @@
+import { create } from 'zustand';
+import { WallType } from '@/types/wall';
+
+export interface WallStoreType {
+  isEdit: boolean;
+  toggleEdit: () => void;
+  wall: WallType;
+  setWall: (states: object) => void;
+  getWall: () => Promise<void>;
+}
+
+export const useWallStore = create<WallStoreType>((set) => ({
+  isEdit: false,
+  toggleEdit: () => set((state) => ({ isEdit: !state.isEdit })),
+
+  wall: {} as WallType,
+
+  getWall: async () => {
+    const response = await fetch('http://localhost:3000/wall');
+    if (response.ok) {
+      set({ wall: await response.json() });
+    }
+  },
+
+  setWall: (states: object) =>
+    set((state) => ({ wall: { ...state.wall, ...states } })),
+}));
