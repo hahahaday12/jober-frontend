9f0cc48ff0abe5f89202868f2029db35a4eef850
howooking <junsgk@gmail.com>
2023-09-11 13:39:31 +0900

    Feat: 리스트 블록 타이틀 수정

--- src/components/wall/blocks/list/ListBlock.tsx
+++ src/components/wall/blocks/list/ListBlock.tsx
@@ -3,72 +3,109 @@ import { useState } from 'react';
 import { Input } from 'antd';
 import { useWallStore } from '@/store';
 import { produce } from 'immer';
-import { ListBlockType, ListType } from '@/types/blocks';
 import { BlockContainer, SingleList } from 'components/index';
+import { BlockType, ListBlockType, ListType } from '@/types/wall';
+import trashIcon from '@/assets/icons/trash.svg';
 
-export const ListBlock = () => {
+interface ListBlockProps {
+  id?: number;
+  blockType?: BlockType;
+}
+export const ListBlock = ({ id, blockType }: ListBlockProps) => {
   const [isListTitleEdit, setIsListTitleEdit] = useState(false);
 
   const { isEdit, wall, setWall } = useWallStore();
+  const targetBlockData = wall.listBlocks?.find((block) => block.id === id);
 
   const handleListTile = (e: React.ChangeEvent<HTMLInputElement>) => {
     setWall(
       produce(wall, (draft) => {
-        (draft.listBlock as ListBlockType).listTitle = e.target.value;
+        const listBlockIndex = (draft.listBlocks as ListBlockType[]).findIndex(
+          (block) => block.id === id,
+        );
+        if (listBlockIndex !== -1) {
+          (draft.listBlocks as ListBlockType[])[listBlockIndex].listTitle =
+            e.target.value;
+        }
       }),
     );
   };
 
-  const handleAddList = () => {
-    setWall(
-      produce(wall, (draft) => {
-        draft.listBlock?.lists.push({
-          id: draft.listBlock.lists[draft.listBlock.lists.length - 1].id + 1,
-          listSubtitle: '',
-          listDescription: '',
-          isLink: false,
-        });
-      }),
-    );
-  };
+  // const handleDeletBlock = () => {
+  //   setWall(
+  //     produce(wall, (draft) => {
+  //       // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  //       (draft as any)[blockName] = undefined;
+  //       draft.order = draft.order.filter((block) => block !== blockName);
+  //     }),
+  //   );
+  // };
+
+  // const handleAddList = () => {
+  //   setWall(
+  //     produce(wall, (draft) => {
+  //       draft.listBlock?.lists.push({
+  //         id: draft.listBlock.lists[draft.listBlock.lists.length - 1].id + 1,
+  //         listSubtitle: '',
+  //         listDescription: '',
+  //         isLink: false,
+  //       });
+  //     }),
+  //   );
+  // };
 
   return (
     <BlockContainer blockName="listBlock">
-      <div className="p-7 flex flex-col gap-5">
-        <div className="flex text-xl font-bold items-center gap-2 text-gray-600">
+      {isEdit && (
+        <img
+          src={trashIcon}
+          alt="trash icon"
+          className="absolute right-[27px] top-[26px] hover:opacity-60 transition cursor-pointer"
+          // onClick={handleDeletBlock}
+        />
+      )}
+
+      <div className="px-[28px] py-[26px] flex flex-col">
+        <div className="flex items-center text-lightBlack gap-[6px] db-20 mb-[16px]">
           {isListTitleEdit ? (
             <Input
-              value={wall.listBlock?.listTitle}
+              placeholder="내용을 입력해주세요."
+              value={targetBlockData?.listTitle}
               onChange={handleListTile}
+              className="db-20 text-lightBlack w-1/3"
             />
           ) : (
-            <h4>{wall.listBlock?.listTitle || '제목'}</h4>
+            <h4>{targetBlockData?.listTitle || '내용을 입력해주세요.'}</h4>
           )}
           {isEdit && (
             <EditOutlined
-              className="cursor-pointer"
+              className="cursor-pointer hover:opacity-60 transition"
               onClick={() => setIsListTitleEdit((prev) => !prev)}
             />
           )}
         </div>
-
-        {wall.listBlock?.lists.map((list: ListType) => (
-          <SingleList
-            key={list.id}
-            isEdit={isEdit}
-            id={list.id}
-            subTitle={list.listSubtitle}
-            desc={list.listDescription}
-            isLink={list.isLink}
-          />
-        ))}
+        <div className="flex flex-col gap-[30px]">
+          {targetBlockData?.lists.map((list: ListType) => (
+            <SingleList
+              key={list.id}
+              isEdit={isEdit}
+              id={list.id}
+              subTitle={list.listSubtitle}
+              desc={list.listDescription}
+              isLink={list.isLink}
+            />
+          ))}
+        </div>
 
         {isEdit && (
           <div className="w-full text-center">
-            <PlusOutlined className="cursor-pointer" onClick={handleAddList} />
+            <PlusOutlined
+              className="cursor-pointer"
+              // onClick={handleAddList}
+            />
           </div>
         )}
       </div>
     </BlockContainer>
   );
-}
+};
