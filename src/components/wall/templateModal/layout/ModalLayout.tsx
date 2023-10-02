e4f97f17e7b859a9049ed70e67133a3c59fd3335
hahahaday12 <hahahaday12@naver.com>
2023-10-02 19:40:15 +0900

    Feat: input창 포커스 될시 조건문 추가

--- src/components/wall/templateModal/layout/ModalLayout.tsx
+++ src/components/wall/templateModal/layout/ModalLayout.tsx
@@ -1,27 +1,26 @@
 import { useCallback, useState } from 'react';
 import { Button, Modal, Select, Input } from 'antd';
 import styled from 'styled-components';
+import { ModalContents } from './modalListContent';
+import { templateText } from '@/textConstants';
+
 import {
   BestTemplate,
   CategoryTemplate,
   SelecteSearchTemplate,
 } from 'components/index';
-import { ModalHeader } from '@/components/common/ModalHeader';
 
 export const ModalOpen = () => {
   const { Search } = Input;
-
-  // modal contents 를 관리하는 state, type 생성
+  // modal contents 를 리하는 state, type 생성
   const [procedure, setProcedure] = useState<
-    'recommand' | 'category' | 'search'
+    'recommand' | 'category' | 'search' | 'select'
   >('recommand');
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-  const [inputText, setInputText] = useState('');
-  //const [keyword, setKeyword] = useState<string>('');
+  const [inputText, setInputText] = useState<string>('');
 
-  // 키값에 맞는 컴포넌트 객체 생성
   const PROCEDURE_MAPPER = {
-    recommand: <BestTemplate />,
+    recommand: <BestTemplate PERSONAL={''} />,
     category: <CategoryTemplate />,
     search: <SelecteSearchTemplate inputText={inputText} />,
   };
@@ -31,20 +30,30 @@ export const ModalOpen = () => {
     setIsModalOpen(true);
   };
 
-  const handleSearchFocus = () => {
-    setProcedure('category');
-  };
-
   const handleOk = () => {
-    setIsModalOpen(false);
+    // 템플릿 선택하고 확인 클릭했을때 자기소개서에 반영이 되야함.
+    //setIsModalOpen(false);
     setInputText('');
     setProcedure('recommand');
+  };
+
+  const handleContents = () => {
+    //alert('클릭');
+    setProcedure('select');
   };
 
   const handleCancel = () => {
     setIsModalOpen(false);
     setInputText('');
     setProcedure('recommand');
+  };
+
+  const handleSearchFocus = () => {
+    if (procedure == 'recommand') {
+      setProcedure('category');
+    } else {
+      setProcedure('search');
+    }
   };
 
   const handleChangeText = useCallback(
@@ -56,51 +65,56 @@ export const ModalOpen = () => {
         setProcedure('category');
       }
     },
-    [],
+    [procedure],
   );
 
   return (
     <>
       <Button className="buttonOpen" type="primary" onClick={showModal}>
-        템플릿 생성
+        {templateText.createTemplate}
       </Button>
       <Modals
         centered
-        title={
-          <ModalHeader
-            title="템플릿 선택하기"
-            handleOk={handleOk}
-            handleCloseModal={handleCancel}
-          />
-        }
-        footer={null}
         open={isModalOpen}
         onOk={handleOk}
         onCancel={handleCancel}
-        closeIcon={null}
+        maskClosable={false}
       >
-        <SettingTemplet>
-          <p className="settingtText">템플릿 설정하기</p>
-          <SelectBox>
-            <InputBox>
-              <Select
-                className="selectbox"
-                defaultValue="문서제목"
-                allowClear
-                options={[{ value: '문서', label: '문서제목' }]}
-              />
-              <Search
-                className="searchBox"
-                type="text"
-                placeholder="input search text"
-                onFocus={handleSearchFocus}
-                value={inputText}
-                onChange={(e) => handleChangeText(e)}
-              />
-            </InputBox>
-            {PROCEDURE_MAPPER[procedure]}
-          </SelectBox>
-        </SettingTemplet>
+        <Button type="primary" onClick={handleContents} className="seeTemplate">
+          {templateText.preview}
+        </Button>
+
+        {procedure === 'select' ? (
+          // "미리보기" 버튼 클릭 시 ModalContents 컴포넌트를 보여줍니다.
+          <ModalContents />
+        ) : (
+          <>
+            {/* 모달이 열릴 때 다른 컴포넌트를 보여줍니다. */}
+            <SettingTemplet>
+              <p className="settingtText">{templateText.settingText}</p>
+              <SelectBox>
+                <InputBox>
+                  <Select
+                    className="selectbox"
+                    defaultValue="문서제목"
+                    allowClear
+                    options={[{ value: '문서', label: '문서제목' }]}
+                  />
+                  <Search
+                    className="searchBox"
+                    type="text"
+                    placeholder="input search text"
+                    onFocus={handleSearchFocus}
+                    value={inputText}
+                    onChange={(e) => handleChangeText(e)}
+                  />
+                </InputBox>
+
+                {PROCEDURE_MAPPER[procedure]}
+              </SelectBox>
+            </SettingTemplet>
+          </>
+        )}
       </Modals>
     </>
   );
@@ -115,18 +129,30 @@ const Modals = styled(Modal)`
     padding-bottom: 106px;
   }
 
-  .ant-btn.css-dev-only-do-not-override-12nk7v7.ant-btn-default {
+  /* .ant-btn css-dev-only-do-not-override-847yc7 ant-btn-default {
     position: absolute;
     top: 40px;
     left: 40px;
     border: 1px solid red;
-    color: red;
-  }
+    background-color: red;
+  } */
 
-  .ant-btn.css-dev-only-do-not-override-12nk7v7.ant-btn-primary {
+  /* .ant-btn.css-dev-only-do-not-override-12nk7v7.ant-btn-primary {
     position: absolute;
     right: 40px;
     top: 40px;
+  } */
+
+  .ant-modal-footer {
+    background-color: pink;
+    top: 10px;
+    position: absolute;
+  }
+
+  .seeTemplate {
+    position: absolute;
+    top: 20px;
+    right: 50px;
   }
 `;
 
@@ -146,16 +172,22 @@ const ModalHeaders = styled.div`
 const SettingTemplet = styled(ModalHeaders)`
   height: 600px;
   border: none;
-  background-color: aliceblue;
   position: relative;
+  top: 50px;
 
   .settingtText {
     width: 112px;
     margin: auto;
-    margin-top: 10px;
-    font-weight: 800;
-    color: blue;
+    margin-top: 14px;
+    font-weight: 500;
+    color: rgba(36, 147, 251, 1);
     position: absolute;
+  }
+
+  .seeTemplate {
+    position: absolute;
+    top: -60px;
+    right: 50px;
   }
 `;
 const SelectBox = styled.div`
