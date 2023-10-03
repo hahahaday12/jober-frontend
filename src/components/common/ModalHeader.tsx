df326f7dbd80b9777d67b56a613edb44ee41c5a6
hahahaday12 <hahahaday12@naver.com>
2023-10-03 15:11:47 +0900

    Chore: 코드 변경사항 저장

--- src/components/common/ModalHeader.tsx
+++ src/components/common/ModalHeader.tsx
@@ -1,13 +1,17 @@
 import { Button, Divider } from 'antd';
+import { CommonModal } from '..';
+import { useState } from 'react';
 
 export const ModalHeader = ({
   handleCloseModal,
+  handleContents,
   handleOk,
   title,
   reset,
 }: {
   handleCloseModal: () => void;
   handleOk: () => void;
+  handleContents: () => void;
   title: React.ReactNode;
   reset?: boolean;
 }) => {
@@ -23,6 +27,11 @@ export const ModalHeader = ({
           </Button>
         )}
         <div className="dm-16">{title}</div>
+
+        <Button type="primary" onClick={handleContents}>
+          미리보기
+        </Button>
+
         <Button type="primary" onClick={handleOk}>
           완료
         </Button>
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,16 @@ import ReactDOM from 'react-dom/client';
 import '@/index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'pages/index';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </>
-)
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </>,
+);
